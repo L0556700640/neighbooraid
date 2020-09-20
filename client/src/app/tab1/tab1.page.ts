d726f3834cf22c730475e56d37a90640d4f3969d
hadarotmi <hadarotmi123@gmail.com>
2020-09-20 20:56:08 +0300

    דפי הרשמת רופא למאגר-סקיצה ראשונית...

--- client/src/app/tab1/tab1.page.ts
+++ client/src/app/tab1/tab1.page.ts
@@ -6,7 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  page1=true;
+  page2=false;
+  page3=false; 
+  finish=false;
+  public form = [
+      { val: 'כויות', isChecked: false },
+      { val: 'חתכים', isChecked: false },
+      { val: 'מכות יבשות', isChecked: false },
+      { val: 'התחשמלות', isChecked: false },
+      { val: 'עקיצה', isChecked: false },
+      { val: 'פריחה', isChecked: false },
+      { val: 'הקשה', isChecked: false },
+      { val: 'נשיכה', isChecked: false },
+      { val: 'התקף חרדה', isChecked: false },
+      { val: 'רעלים', isChecked: false },
+      { val: 'חום', isChecked: false }
+    ];
   constructor() {}
 
+  next1()
+  {
+    this.page1=false;
+    this.page2=true;
+  }
+  next2()
+  {
+    this.page2=false;
+    this.page3=true;
+  }
+
+  next3()
+  {
+    this.page3=false;
+    this.finish=true;
+  }
+
+  previous1()
+  {
+    this.page1=true;
+    this.page2=false;
+  }
+
+  previous2()
+  {
+    this.page2=true;
+    this.page3=false;
+  }
 }
+
