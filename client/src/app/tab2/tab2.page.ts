b9237626e82d4be10a118c6728e9a8b9e8ad9c73
L0556700640 <l0556700640@gmail.com>
2020-09-22 15:12:38 +0300

    רשימות מקרים רופא וחולה לירז

--- client/src/app/tab2/tab2.page.ts
+++ client/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cases } from '../shared/models/cases.model';
+import { CasesService } from '../shared/services/cases.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  searchPage = true;
+  familyPage = false;
+  doctorsPage = false;
+  responsePage = false;
+  allCases: Cases[] = [];
+  constructor(private casesService: CasesService) {
+    this.casesService.getAllCases().subscribe(res => { this.allCases = res; });
+  }
+  next1() {
+    this.doctorsPage = true;
+    this.searchPage = false;
 
-  constructor() {}
-
+  }
 }
