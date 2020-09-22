1facbc3ccbaf87a6ff6de2d782b8338541f9714a
hadarotmi <hadarotmi123@gmail.com>
2020-09-22 16:46:41 +0300

    שינויים בהרשמת רופא

--- client/src/app/tab1/tab1.page.ts
+++ client/src/app/tab1/tab1.page.ts
@@ -13,43 +13,53 @@ import { CasesService } from '../shared/services/cases.service';
 export class Tab1Page {
   page1 = true;
   page2 = false;
-  page3 = false;
   finish = false;
- allCases:Cases[]=[];
- doctor: DoctorDetails= new DoctorDetails();
+  imagePath;
+  imageProvider;
+  allCases:Cases[]=[];
+  chooseCases=[]
+  doctor: DoctorDetails= new DoctorDetails();
   constructor(private doctorService: DoctorService,private casesService:CasesService ) { 
    this.casesService.getAllCases().subscribe(res=>{this.allCases=res;});
   }
 
-  next1() {
-    this.page1 = false;
-    this.page2 = true;
-    this.doctor.Doctor.isConfirmed = false;
+  next() {
+    if(this.page1==true)
+    {
+      this.page1 = false;
+      this.page2 = true;
+      this.doctor.Doctor.isConfirmed = false;
+    }
+    else
+    {
+      this.page2 = false;
+      this.finish = true; 
+      this.doctorService.addDoctor(this.doctor).subscribe(
+        (res)=>{alert(res)}
+      )
+    }
+    
   
   }
 
-  next2() {
-    this.page2 = false;
-    this.page3 = true;
-  }
-
-  next3() {
-    
-    this.doctorService.addDoctor(this.doctor).subscribe(
-      (res)=>{alert(res)}
-    )
-    this.page3 = false;
-    this.finish = true;
-  }
-
-  previous1() {
+  previous() {
     this.page1 = true;
     this.page2 = false;
   }
+  // next3() {
+  //   this.page3 = false;
+  //   this.finish = true;
+  // }
 
-  previous2() {
-    this.page2 = true;
-    this.page3 = false;
-  }
+  // previous2() {
+  //   this.page2 = true;
+  //   this.page3 = false;
+  // }
+
+  changeListener($event): void {
+    this.imagePath = $event.target.files[0];
+    console.log($event.target.files[0])
+    //this.imageProvider.uploadImage(this.imagePath)
+}
 }
 
