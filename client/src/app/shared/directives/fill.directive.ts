6c1dffb6c9eb14ca25150f4ca952a688359c8542
Liraz <l0556700640@gmail.com>
2021-02-15 16:36:34 +0200

    ששש

--- client/src/app/shared/directives/fill.directive.ts
+++ client/src/app/shared/directives/fill.directive.ts
@@ -6,14 +6,13 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class FillDirective {
 
 
-@Input('appFill') set fill(val){
-  this.fillElement(val)
-}
+  @Input('appFill') set fill(val) {
+    this.fillElement(val)
+  }
   constructor(private el: ElementRef) {
-    el.nativeElement.style.width = `${this.fill}%` 
-   }
-  fillElement(val)
-{
-  this.el.nativeElement.style.width = `${val}%`
-}
+    el.nativeElement.style.width = `${this.fill}%`
+  }
+  fillElement(val) {
+    this.el.nativeElement.style.width = `${val}%`
+  }
 }
